fa1e74ce748dd3535bf0db6f678541577c3a674e
Clarence Zhou <zhou_chenz@163.com>
2024-04-15 00:08:11 +0800

    add 2ed way for cross compile

--- cross_compile_build.sh
+++ cross_compile_build.sh
@@ -6,3 +6,12 @@ export CC=aarch64-linux-gnu-gcc
 export CXX=aarch64-linux-gnu-g++
 cmake ..
 make install
+
+
+############################################
+
+# 2ed way for croos compile
+mkdir -p build && cd build
+export PATH=/path/to/the/gcc-linaro-xxxx-x86_64_aarch64-linux-gnu/bin:$PATH
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/aarch64-linux-gnu.cmake ..
+make install
