de9a5477a2a8b70f09bdbf77b435fc16ce5228d0
Clarence Zhou <zhou_chenz@163.com>
2024-04-22 23:27:13 +0800

    add pkgconfig for base lib

--- pkg_dep_install.sh
+++ pkg_dep_install.sh
@@ -15,3 +15,13 @@ sudo apt install package1 package2 package3
 # Add any additional packages you want to install here
 
 echo "Package installation complete."
+
+# build 3rd party packages
+
+# build spdlog
+cmake .. -DCMAKE_INSTALL_PREFIX=./install -DSPDLOG_BUILD_BENCH=ON -DSPDLOG_BUILD_SHARED=ON
+make -j8 install
+
+# build cpp-tbox
+cmake .. -DCMAKE_INSTALL_PREFIX=./install
+make -j8 install
