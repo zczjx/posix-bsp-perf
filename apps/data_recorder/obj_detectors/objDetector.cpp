fdeb26ec460e89ec59a276926ecfd61ee4002926
Clarence Zhou <zhou_chenz@163.com>
2025-07-01 00:36:23 +0800

    data_recorder: bring up the obj_detectors

--- apps/data_recorder/obj_detectors/objDetector.cpp
+++ apps/data_recorder/obj_detectors/objDetector.cpp
@@ -1,4 +1,7 @@
 #include "objDetector.hpp"
+#include <common/msg/ObjDetectMsg.hpp>
+#include <common/msg/CameraSensorMsg.hpp>
+
 #include <thread>
 #include <chrono>
 
@@ -7,12 +10,25 @@ namespace apps
 namespace data_recorder
 {
 
-ObjDetector::ObjDetector(const json& nodes_ipc)
+ObjDetector::ObjDetector(ArgParser&& args, const json& nodes_ipc)
 {
     for (const auto& node: nodes_ipc["object_detector"])
     {
         std::string name{node["name"]};
         std::string status{node["status"]};
+
+        const json& subscriber = node["subscriber"];
+        const json& publisher = node["publisher"];
+
+        m_input_shmem_port = std::make_shared<SharedMemSubscriber>(subscriber["topic"],
+                                                                   subscriber["shmem"],
+                                                                   subscriber["shmem_slots"],
+                                                                   subscriber["shmem_single_buffer_size"]);
+
+        m_output_shmem_port = std::make_shared<SharedMemPublisher>(publisher["topic"],
+                                                                   publisher["shmem"],
+                                                                   publisher["shmem_slots"],
+                                                                   publisher["shmem_single_buffer_size"]);
     }
 }
 
--- apps/data_recorder/obj_detectors/objDetector.hpp
+++ apps/data_recorder/obj_detectors/objDetector.hpp
@@ -3,8 +3,15 @@
 
 #include <string>
 #include <nlohmann/json.hpp>
+#include <zeromq_ipc/sharedMemSubscriber.hpp>
+#include <zeromq_ipc/sharedMemPublisher.hpp>
+#include <shared/BspLogger.hpp>
+#include <shared/ArgParser.hpp>
+#include <bsp_dnn/dnnObjDetector.hpp>
 
 using json = nlohmann::json;
+using namespace midware::zeromq_ipc;
+using namespace bsp_perf::shared;
 
 namespace apps
 {
@@ -14,11 +21,14 @@ namespace data_recorder
 class ObjDetector
 {
 public:
-    ObjDetector(const json& nodes_ipc);
+    ObjDetector(ArgParser&& args, const json& nodes_ipc);
 
     void runLoop();
 
 private:
+    std::shared_ptr<SharedMemSubscriber> m_input_shmem_port;
+    std::shared_ptr<SharedMemPublisher> m_output_shmem_port;
+    std::unique_ptr<bsp_dnn::dnnObjDetector> m_dnnObjDetector;
 };
 
 } // namespace data_recorder
